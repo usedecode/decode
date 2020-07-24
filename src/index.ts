95541a2990d88fea6fd55fce0c89d21b21893e06
Anthony Accomazzo <accomazz@gmail.com>
2020-07-24 13:56:08 -0700

    v0.1.4

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./useFetcher";
 export { default as DecodeProvider, useToken } from "./DecodeProvider";
 export { default as ShowDecodeError } from "./ShowDecodeError";
 export { default as ShowDecodeLoading } from "./ShowDecodeLoading";
+export { default as useDecode } from "./useDecode";
 export * from "./useDecode";
 export { mutate } from "swr";
--- src/useDecode.ts
+++ src/useDecode.ts
@@ -1,4 +1,3 @@
-import { useRef } from "react";
 import { useFetcher } from "./useFetcher";
 import useSWR, { responseInterface, ConfigInterface } from "swr";
 
